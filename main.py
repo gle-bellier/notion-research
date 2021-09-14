eac29934bdbc034a558e470e0b68b419c9c35f87
Erik <glebellier@protonmail.com>
2021-09-14 11:56:44 +0200

    Add bibtex export in bib file

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import textract
 from reader import Article
 
 if __name__ == "__main__":
-
-    bibtex_file = ""
     PATH = ""
-    name = "article2.pdf"
+    SAVING_PATH = "\\articles"
+
+    bibtex_file = "biblio.bib"
+
+    name = "article.pdf"
 
     text = str(textract.process(PATH + name))
     article = Article(text)
     print(article.metadata)
 
-    database_id = "db99131281c946e2b9aa50af8bc08577"
-    data = {
-        "title": article.metadata["title"],
-        "authors": article.metadata["authors"],
-        "date": article.metadata["year"],
-        "abstract": article.metadata["abstract"],
-    }
+    # Add bibtex to the bib file :
+    with open(SAVING_PATH + bibtex_file, "a") as file:
+        file.write(article.metadata["bibtex"] + "\n")
 
-    API.addArticle(database_id, data)
+    # database_id = "db99131281c946e2b9aa50af8bc08577"
+    # data = {
+    #     "title": article.metadata["title"],
+    #     "authors": article.metadata["authors"],
+    #     "date": article.metadata["year"],
+    #     "abstract": article.metadata["abstract"],
+    # }
+
+    # API.addArticle(database_id, data)
